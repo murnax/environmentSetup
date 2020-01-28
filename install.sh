94fb703c2df18ab4fd0ae5c06d6b54a78271db54
Suratin Pattanawongthai <suratin.elec@gmail.com>
2020-01-28 13:53:59 +0700

    update

--- install.sh
+++ install.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
+BOLD=$(tput bold)
+CYAN=$(tput setaf 6)
 YELLOW=$(tput setaf 3)
 
+ARROW="$CYAN$BOLD==>$DEFAULT"
 ARROW_YELLOW="$YELLOW$BOLD==>$DEFAULT"
 
-CASK_APPLICATIONS=(google-chrome postman slack git sourcetree visual-studio-code docker evernote)
-VS_CODE_EXTENSIONS=(eamodio.gitlens donjayamanne.githistory esbenp.prettier-vscode)
+BREW_APPLICATIONS=(jq git)
+CASK_APPLICATIONS=(google-chrome postman slack sourcetree visual-studio-code docker evernote iterm2 android-file-transfer the-unarchiver)
+VS_CODE_EXTENSIONS=(eamodio.gitlens donjayamanne.githistory esbenp.prettier-vscode VisualStudioExptTeam.vscodeintellicode)
+NPM_GLOBAL_MODULES=(typescript ts-node pm2)
 
 # Homebrew
 if $IS_HOMEBREW_INSTALLED; then
-    brew update
-    brew upgrade
+    # brew update
+    # brew upgrade
     echo "Homebrew is already installed"
 else 
     echo "${ARROW} Installing Homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+#-------------------
+# Brew applications
+#-------------------
+for application in "${BREW_APPLICATIONS[@]}"; do
+    echo "${ARROW} Installing ${application} via Homebrew..."
+    brew install ${application}
+done
 
 #-------------------
 # Cask applications
@@ -28,6 +41,10 @@ done
 #------------------------------------
 # VS Code Configuration & Extensions
 #------------------------------------
+for extension in "${VS_CODE_EXTENSIONS[@]}"; do
+    echo "${ARROW} Installing ${extension} to VS Code..."
+    code --install-extension ${extension}
+done
 
 
 
