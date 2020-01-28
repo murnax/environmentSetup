089a4c79d766d9dc25152fc4d3a680a91f0f3f09
Suratin Pattanawongthai <suratin.elec@gmail.com>
2020-01-28 09:44:25 +0700

    init

---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+YELLOW=$(tput setaf 3)
+
+ARROW_YELLOW="$YELLOW$BOLD==>$DEFAULT"
+
+CASK_APPLICATIONS=(google-chrome postman slack git sourcetree visual-studio-code docker evernote)
+NODE
+VS_CODE_EXTENSIONS=(eamodio.gitlens esbenp.prettier-vscode)
+
+IS_HOMEBREW_INSTALLED=false
+
+# if hash brew 2>/dev/null; then
+#     IS_HOMEBREW_INSTALLED=true
+# fi
+
+# Homebrew
+if $IS_HOMEBREW_INSTALLED; then
+    brew update
+    brew upgrade
+    echo "Homebrew is already installed"
+else 
+    echo "${ARROW} Installing Homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+#-------------------
+# Cask applications
+#-------------------
+for application in "${CASK_APPLICATIONS[@]}"; do
+    echo "${ARROW} Installing ${application} via Homebrew cask..."
+    brew cask install ${application}
+done
+
+#------------------------------------
+# VS Code Configuration & Extensions
+#------------------------------------
+
+
+# Node version manager
+if ! [ -x "$(command -v nvm)" ]; then
+    echo "NVM is already installed"
+else
+    echo "${ARROW} Installing Node version manager..."
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+    source ~/.bash_profile  
+fi
+
+# Node
+if hash node; then
+    echo "Node is already installed"
+else
+    echo "${ARROW} Installing Node..."
+    nvm install --lts
+fi
+
+# Node global packages
